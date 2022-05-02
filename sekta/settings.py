6d8df9182b02bda676f4e71a5c2445906a9d1899
ARaimbekov <56764407+ARaimbekov@users.noreply.github.com>
2022-05-02 17:14:02 +0700

    Update settings.py

--- sekta/settings.py
+++ sekta/settings.py
@@ -74,6 +74,18 @@ WSGI_APPLICATION = 'sekta.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
 
