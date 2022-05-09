708bd8adb13846353e742d285174d8eb908e3cb6
araimbekov <azim28021975>
2022-05-09 15:18:08 +0700

    mysql

--- sekta/settings.py
+++ sekta/settings.py
@@ -77,13 +77,8 @@ WSGI_APPLICATION = 'sekta.wsgi.application'
 
 DATABASES = {
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sekta_dev',
-        'USER': 'sektant',
-        'PASSWORD': 'sektant',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
