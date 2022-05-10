9bdfd08e68cf469cd448aaa4596a6d35a0c9eaca
Azim <raim.azim@mail.ru>
2022-05-09 23:00:04 -0700

    app add

---
+++ app/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+until nc -z -v -w30 mysql 3306
+do
+  echo "Waiting for database connection..."
+  sleep 1
+done
+
+python3 manage.py makemigrations main
+python3 manage.py migrate --noinput
+python3 manage.py runserver 0.0.0.0:8080 --insecure
