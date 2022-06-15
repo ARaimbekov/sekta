ec1bf489e68c15fbb41033af73e8bb315fd76c98
meSerge8 <melnikov.serge8@gmail.com>
2022-06-16 00:23:55 +0700

    [vacancies] add listing

--- services/vacancies/main.c
+++ services/vacancies/main.c
@@ -7,14 +7,31 @@
 
 #define PORT 8080
 #define FAIL -1
+
+#define LOGO_IMG "logo_img.txt"
+#define VACANCIES_IMG "vacancies_img.txt"
+
 #define VACANCY_MEMORY 2048
+#define VACANCIES_AMOUNT 40
+#define VACANCY_FORMAT "#%d\nName: %s\nDescription: %s\n\n"
 
 int server_fd;
 struct sockaddr_in address;
+struct vacancy
+{
+    char *name;
+    char *description;
+};
 
 void setup();
 void serve(int socket_fd);
-int sendLogo(int socket_fd);
+int printImg(int socket_fd, char *path);
+int printVacanciesImg(int socket_fd);
+
+int scanVacancies(struct vacancy vs[VACANCIES_AMOUNT]);
+int scanVacancy(FILE *file, struct vacancy *vs);
+
+int printVacancies(int socket_fd, int amount, struct vacancy vs[VACANCIES_AMOUNT]);
 
 int main(int argc, char const *argv[])
 {
@@ -70,20 +87,29 @@ void setup()
 
 void serve(int socket_fd)
 {
-    if (sendLogo(socket_fd) == FAIL)
-    {
+    if (printImg(socket_fd, LOGO_IMG) == FAIL)
         return;
-    }
 
-    int valread;
-    char buffer[1024] = {0};
-    valread = read(socket_fd, buffer, 1024);
-    printf("%s\n", buffer);
+    while (1)
+    {
+        struct vacancy vs[VACANCIES_AMOUNT];
+        int size = scanVacancies(vs);
+        if (size == FAIL)
+            break;
+
+        if (printVacancies(socket_fd, size, vs) == FAIL)
+            break;
+
+        int valread;
+        char buffer[1024] = {0};
+        valread = read(socket_fd, buffer, 1024);
+        printf("%s\n", buffer);
+    }
 }
 
-int sendLogo(int socket_fd)
+int printImg(int socket_fd, char *path)
 {
-    FILE *file = fopen("img.txt", "r");
+    FILE *file = fopen(path, "r");
     if (NULL == file)
     {
         printf("Err: file img.txt can't be opened \n");
@@ -111,7 +137,7 @@ int sendLogo(int socket_fd)
     return 0;
 }
 
-int printVacancies(int socket_fd)
+int scanVacancies(struct vacancy vs[VACANCIES_AMOUNT])
 {
     FILE *file = fopen("vacancies.txt", "r");
     if (NULL == file)
@@ -120,12 +146,59 @@ int printVacancies(int socket_fd)
         return FAIL;
     }
 
-    for (int i = 0; i < 40; i++)
+    for (int i = 0; i < VACANCIES_AMOUNT; i++)
+        if (scanVacancy(file, &vs[i]) == FAIL)
+            return i - 1;
+}
+
+int scanVacancy(FILE *file, struct vacancy *vs)
+{
+    vs->name = malloc(100);
+    int i;
+
+    for (i = 0; i < 100; i++)
     {
-        // alloc memory
-        // scan
-        // send
-        // free
+        vs->name[i] = fgetc(file);
+
+        if (vs->name[i] == EOF)
+            return FAIL;
+
+        if (vs->name[i] == ' ')
+            break;
+    }
+    vs->name[i] = 0;
+
+    vs->description = malloc(200);
+    for (i = 0; i < 200; i++)
+    {
+        vs->description[i] = fgetc(file);
+
+        if (vs->description[i] == EOF)
+            return FAIL;
+
+        if (vs->description[i] == '\n')
+            break;
+    }
+    vs->description[i] = 0;
+
+    return 0;
+}
+
+int printVacancies(int socket_fd, int amount, struct vacancy vs[VACANCIES_AMOUNT])
+{
+    if (printImg(socket_fd, VACANCIES_IMG) == FAIL)
+        return FAIL;
+
+    for (int i = 0; i < amount; i++)
+    {
+        char str[300];
+        int size = sprintf(str, VACANCY_FORMAT, i, vs[i].name, vs[i].description);
+
+        if (send(socket_fd, str, size, 0) == FAIL)
+        {
+            printf("Err: sending vacancy failed \n");
+            return FAIL;
+        }
     }
 
     return 0;
